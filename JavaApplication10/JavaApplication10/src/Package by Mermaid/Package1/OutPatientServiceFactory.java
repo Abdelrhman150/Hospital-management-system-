f891a6dfac59bea8a80af5ed3a6346b4682497ba
AbdoHossam <Abdelrhmanelgaml85@gmail.com>
2026-03-26 21:43:26 +0200

    DONEEEEEEEEEEE

--- JavaApplication10/JavaApplication10/src/Package by Mermaid/Package1/OutPatientServiceFactory.java
+++ JavaApplication10/JavaApplication10/src/Package by Mermaid/Package1/OutPatientServiceFactory.java
@@ -2,13 +2,15 @@ package Package1;
 
 public class OutPatientServiceFactory implements HospitalServicFactory {
     @Override
-    public Bill createBill(String patientId , int daysOfStay) {
-        return new VisitingBill();
+    public Bill createBill(String patientId, int daysOfStay) {
+        VisitingBill bill = new VisitingBill();
+        bill.generateBill(patientId, daysOfStay, null); // room is null for visiting
+        return bill;
     }
 
     @Override
     public Appointment createAppointment(String patientId, String doctorName, String appointmentDate) {
         return new VistingAppointment();
-        
+
     }
 }
